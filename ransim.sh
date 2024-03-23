37461d27c5a8665941a62c99a3ab2deec3e85366
Ko4la <francois@le.ko4la.fr>
2024-03-23 13:50:26 +0100

    Create ransim.sh

---
+++ ransim.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# goal:
+# This script sets up a demo to demonstrate Rubrik Anomaly Detection
+#
+# usage:
+#  sudo curl https://raw.githubusercontent.com/francois-le-ko4la/LABs/master/ransim.sh | sudo sh
+#
+
+RBK_PATH="/opt/rubrik/scripts"
+URL="https://raw.githubusercontent.com/francois-le-ko4la/LABs/master"
+
+mkdir -p $RBK_PATH
+python3 -m venv $RBK_PATH/venv
+$RBK_PATH/venv/bin/python -m pip install cryptography
+wget -O $RBK_PATH/encrypt_file.py $URL/encrypt_file.py
+wget -O $RBK_PATH/key $URL/key
+chmod 754 $RBK_PATH/encrypt_file.py
