e2a42dfe38320fbd196473f29760e902d28585ed
Ko4la <francois@le.ko4la.fr>
2024-03-27 10:45:24 +0100

    Update ransim.sh

--- ransim.sh
+++ ransim.sh
@@ -10,10 +10,16 @@
 RBK_PATH="/opt/rubrik/scripts"
 URL="https://raw.githubusercontent.com/francois-le-ko4la/LABs/master"
 
-apt install -y python3-full
+echo "Install python3-full..."
+apt-get install -y python3-full &> /dev/null
+echo "Create scripts repository..."
 mkdir -p $RBK_PATH
-python3 -m venv $RBK_PATH/venv
-$RBK_PATH/venv/bin/python -m pip install cryptography
-wget -O $RBK_PATH/encrypt_file.py $URL/encrypt_file.py
-wget -O $RBK_PATH/key $URL/key
-chmod 754 $RBK_PATH/encrypt_file.py
+echo "Create python venv..."
+python3 -m venv $RBK_PATH/venv &> /dev/null
+echo "Install cryptography lib..."
+$RBK_PATH/venv/bin/python -m pip install cryptography &> /dev/null
+echo "Download crypto script..."
+wget -O $RBK_PATH/encrypt_file.py $URL/encrypt_file.py &> /dev/null
+wget -O $RBK_PATH/key $URL/key &> /dev/null
+chmod 754 $RBK_PATH/encrypt_file.py &> /dev/null
+echo "Operation completed successfully."
