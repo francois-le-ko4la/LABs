8354d59b9a6b9826d1a22f03e326630636a7d7b2
Ko4la <francois@le.ko4la.fr>
2024-03-27 14:40:14 +0100

    Create gen_file.sh

---
+++ gen_file.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DEST="/home/shares"
+
+wget https://github.com/francois-le-ko4la/LABs/blob/master/lorem.txt
+mkdir -p $DEST
+for i in {1..4096}; do cp lorem.txt $DEST/lorem$i.txt; done
+ 
