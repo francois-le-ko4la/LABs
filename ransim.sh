ff72279ce32ff9e21be55d8fa694a1b8199082d1
Ko4la <francois@le.ko4la.fr>
2024-03-27 09:15:28 +0100

    Update ransim.sh

--- ransim.sh
+++ ransim.sh
@@ -10,7 +10,7 @@
 RBK_PATH="/opt/rubrik/scripts"
 URL="https://raw.githubusercontent.com/francois-le-ko4la/LABs/master"
 
-apt install python3-full
+apt install python3-full -y
 mkdir -p $RBK_PATH
 python3 -m venv $RBK_PATH/venv
 $RBK_PATH/venv/bin/python -m pip install cryptography
