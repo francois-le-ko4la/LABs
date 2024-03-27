15689447ee6c1dd6f97424b1dc3185e43baca997
Ko4la <francois@le.ko4la.fr>
2024-03-27 11:05:34 +0100

    Update ransim.sh

--- ransim.sh
+++ ransim.sh
@@ -10,16 +10,16 @@
 RBK_PATH="/opt/rubrik/scripts"
 URL="https://raw.githubusercontent.com/francois-le-ko4la/LABs/master"
 
-echo "Install python3-full..."
+echo "$(date --iso-8601=seconds) - RANSIM - Install python3-full..."
 apt-get install -y python3-full > /dev/null 2>&1
-echo "Create scripts repository..."
+echo "$(date --iso-8601=seconds) - RANSIM - Create scripts repository..."
 mkdir -p $RBK_PATH
-echo "Create python venv..."
+echo "$(date --iso-8601=seconds) - RANSIM - Create python venv..."
 python3 -m venv $RBK_PATH/venv > /dev/null 2>&1
-echo "Install cryptography lib..."
+echo "$(date --iso-8601=seconds) - RANSIM - Install cryptography lib..."
 $RBK_PATH/venv/bin/python -m pip install cryptography > /dev/null 2>&1
-echo "Download crypto script..."
+echo "$(date --iso-8601=seconds) - RANSIM - Download crypto script..."
 wget -q -O $RBK_PATH/encrypt_file.py $URL/encrypt_file.py > /dev/null 2>&1
 wget -q -O $RBK_PATH/key $URL/key > /dev/null 2>&1
 chmod 754 $RBK_PATH/encrypt_file.py > /dev/null 2>&1
-echo "Operation completed successfully."
+echo "$(date --iso-8601=seconds) - RANSIM - Operation completed successfully."
