23652d9e3f09371119f0b51020b583c33d776853
Ko4la <francois@le.ko4la.fr>
2024-04-07 18:54:46 +0200

    Update gen_file.sh

--- gen_file.sh
+++ gen_file.sh
@@ -2,7 +2,7 @@
 
 DEST="/home/shares"
 
-wget https://github.com/francois-le-ko4la/LABs/blob/master/lorem.txt
+wget https://raw.githubusercontent.com/francois-le-ko4la/LABs/master/lorem.txt
 mkdir -p $DEST
 for i in {1..4096}; do cp lorem.txt $DEST/lorem$i.txt; done
  
