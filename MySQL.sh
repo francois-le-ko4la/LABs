870207dd1b0301ee4ced878a89cfebd1be3691f6
Ko4la <francois@le.ko4la.fr>
2024-04-07 19:11:39 +0200

    Update MySQL.sh

--- MySQL.sh
+++ MySQL.sh
@@ -1,25 +1,37 @@
 #!/bin/sh
 #
-# goal:
+# DESCRIPTION:
 # This script sets up a demo MySQL database with a service account
 # and a dump script.
 #
-# usage:
-#  sudo curl https://raw.githubusercontent.com/francois-le-ko4la/LABs/master/MySQL.sh | sudo sh
+# DISCLAIMER:
+# This script is developed for demonstration purposes only and should be used
+# with caution. Always ensure the security of your data and use appropriate
+# encryption methods in production environments.
+# This script is not supported under any support program or service. 
+# All scripts are provided AS IS without warranty of any kind. 
+# The author further disclaims all implied warranties including, without
+# limitation, any implied warranties of merchantability or of fitness for a
+# particular purpose. 
+# The entire risk arising out of the use or performance of the sample scripts
+# and documentation remains with you. 
+# In no event shall its authors, or anyone else involved in the creation,
+# production, or delivery of the scripts be liable for any damages whatsoever 
+# (including, without limitation, damages for loss of business profits, business
+# interruption, loss of business information, or other pecuniary loss) 
+# arising out of the use of or inability to use the sample scripts or documentation,
+# even if the author has been advised of the possibility of such damages.
 #
-# linux tested:
+# REQUIREMENTS:
 # - ubuntu 20.04+
+#
+# SETUP:
+#  sudo curl https://raw.githubusercontent.com/francois-le-ko4la/LABs/master/MySQL.sh | sudo sh
 #
 # After running the script, you'll have:
 # - A MySQL database named "employees"
 # - A backup account rubrik_svc/Rubrik@123!
 # - An example dump script
-#
-# DISCLAIMER:
-# This script should not be used in a production environment without
-# appropriate evaluation and additional modifications.
-# The author of this script cannot be held responsible for any
-# potential damages or losses resulting from its use.
 #
 
 SCRIPT_DIR="/opt/rubrik/scripts"
