cd3363e70b028c6aa22ddb90999fb7a7922493c4
Ko4la <francois@le.ko4la.fr>
2024-04-10 04:23:41 +0200

    Update BuildMSSQL.ps1

--- BuildMSSQL.ps1
+++ BuildMSSQL.ps1
@@ -1,60 +1,120 @@
-function Install-SQLServerExpress2019 {
+<#
+.SYNOPSIS
+    Script to automate the installation of SQL Server Express 2019, SQL Server Management Studio (SSMS),
+    and restore the AdventureWorks2019 database.
+
+.DESCRIPTION
+    This script automates the installation of SQL Server Express 2019 and SQL Server Management Studio (SSMS),
+    and restores the AdventureWorks2019 database for testing or development purposes only.
+
+    Prerequisites:
+    - Administrator privileges on the machine.
+    - Internet connection to download SQL Server Express 2019 and SSMS.
+    - PowerShell 5.1 or later.
+
+.PARAMETER None
+    This script does not accept any parameters.
+
+.NOTES
+    File Name: BuildMSSQL.ps1
+
+.EXAMPLE
+    PS C:\> .\BuildMSSQL.ps1
+    # Runs the script to install SQL Server Express 2019, SSMS, and restore AdventureWorks2019 database.
+
+#>
+
+
+###############################################################################
+# Define variables
+###############################################################################
+$ServerInstance = "localhost\SQLEXPRESS"
+$DatabaseName = "AdventureWorks2019"
+$UserMssql = "RUBRIK\demo"
+$MssqlRoot = "C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL"
+$AdventureWorkUrl = "https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksLT2019.bak"
+$MssqlUrl = "https://go.microsoft.com/fwlink/?linkid=866658"
+$MssqlBin = "SQL2019-SSEI-Expr.exe"
+$SsmsUrl = "https://go.microsoft.com/fwlink/?linkid=2257624&clcid=0x409"
+$SsmsBin = "SSMS-Setup-ENU.exe"
+
+
+###############################################################################
+# Function to install SQL Server Express 2019
+###############################################################################
+function Install-SqlServerExpress2019 {
     Write-Host "Downloading SQL Server Express 2019..."
     $Path = $env:TEMP
-    $Installer = "SQL2019-SSEI-Expr.exe"
-    $URL = "https://go.microsoft.com/fwlink/?linkid=866658"
-    Invoke-WebRequest $URL -OutFile $Path\$Installer
+    $Installer = $MssqlBin
+    $Url = $MssqlUrl
+    Invoke-WebRequest $Url -OutFile "$Path\$Installer"
 
     Write-Host "Installing SQL Server Express..."
-    Start-Process -FilePath $Path\$Installer -Args "/ACTION=INSTALL /IACCEPTSQLSERVERLICENSETERMS /QUIET" -Verb RunAs -Wait
-    Remove-Item $Path\$Installer
+    Start-Process -FilePath "$Path\$Installer" -Args "/ACTION=INSTALL /IACCEPTSQLSERVERLICENSETERMS /QUIET" -Verb RunAs -Wait
+    Remove-Item "$Path\$Installer"
 }
 
-function Install-MSSP {
-    Write-Host "Downloading MSSP..."
+
+###############################################################################
+# Function to install SQL Server Management Studio (SSMS)
+###############################################################################
+function Install-Ssms {
+    Write-Host "Downloading SSMS..."
     $Path = $env:TEMP
-    $Installer = "SSMS-Setup-ENU.exe"
-    $URL = "https://go.microsoft.com/fwlink/?linkid=2257624&clcid=0x409"
-    Invoke-WebRequest $URL -OutFile $Path\$Installer
+    $Installer = $SsmsBin
+    $Url = $SsmsUrl
+    Invoke-WebRequest $Url -OutFile "$Path\$Installer"
 
-    Write-Host "Installing MSSP..."
-    Start-Process -FilePath $Path\$Installer -Args "/Install /Quiet /NorestartT" -Verb RunAs -Wait
-    Remove-Item $Path\$Installer
+    Write-Host "Installing SSMS..."
+    Start-Process -FilePath "$Path\$Installer" -Args "/Install /Quiet /NorestartT" -Verb RunAs -Wait
+    Remove-Item "$Path\$Installer"
 }
 
-Install-SQLServerExpress2019
-Install-MSSP
 
-$AdventureWork="https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksLT2019.bak"
-$backupPath = "C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\Backup\AdventureWorksLT2019.bak"
-Invoke-WebRequest -Uri $AdventureWork -OutFile $backupPath
+###############################################################################
+# Function to restore the database
+###############################################################################
+function Restore-Database {
+    param (
+        [string]$ServerInstance,
+        [string]$DatabaseName,
+        [string]$BackupUrl,
+        [string]$UserMssql,
+        [string]$MssqlRoot
+    )
 
+    # Download the backup file
+    $BackupPath = Join-Path -Path $MssqlRoot -ChildPath "Backup\$DatabaseName.bak"
+    Invoke-WebRequest -Uri $BackupUrl -OutFile $BackupPath
 
-Install-Module -Name SqlServer -Force -AllowClobber
-Import-Module SqlServer
+    # Restore script
+    $SqlQueryRestoreDb = @"
+    USE master;
+    GO
+    DECLARE @BackupPath NVARCHAR(500) = '$BackupPath';
 
-$serverInstance = "localhost\SQLEXPRESS"
-$server = New-Object Microsoft.SqlServer.Management.Smo.Server($serverInstance)
-$server.Databases
-Invoke-Sqlcmd -ServerInstance $serverInstance -Query "EXEC sp_addsrvrolemember 'RUBRIK\demo', 'sysadmin'" -TrustServerCertificate
+    IF DB_ID('$DatabaseName') IS NOT NULL
+    BEGIN
+        ALTER DATABASE $DatabaseName SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+        DROP DATABASE $DatabaseName;
+    END
 
-$sqlQuery_restore_db = @"
-USE master;
-GO
-DECLARE @BackupPath NVARCHAR(500) = 'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\backup\AdventureWorksLT2019.bak';
-
-IF DB_ID('AdventureWorks2019') IS NOT NULL
-BEGIN
-    ALTER DATABASE AdventureWorks2019 SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE AdventureWorks2019;
-END
-
-RESTORE DATABASE AdventureWorks2019
-FROM DISK = @BackupPath
-WITH MOVE 'AdventureWorksLT2019_Data' TO 'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\AdventureWorksLT2019.mdf',
-     MOVE 'AdventureWorksLT2019_Log' TO 'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\AdventureWorksLT2019.ldf',
-     REPLACE;
-ALTER DATABASE AdventureWorks2019 SET RECOVERY Full
+    RESTORE DATABASE $DatabaseName
+    FROM DISK = @BackupPath
+    WITH MOVE 'AdventureWorksLT2019_Data' TO '$MssqlRoot\DATA\$DatabaseName.mdf',
+         MOVE 'AdventureWorksLT2019_Log' TO '$MssqlRoot\DATA\$DatabaseName.ldf',
+         REPLACE;
+    ALTER DATABASE $DatabaseName SET RECOVERY Full
 "@
-Invoke-Sqlcmd -ServerInstance $serverInstance -Query $sqlQuery_restore_db -TrustServerCertificate
+    Invoke-Sqlcmd -ServerInstance $ServerInstance -Query $SqlQueryRestoreDb -TrustServerCertificate
+}
 
+
+###############################################################################
+# MAIN
+###############################################################################
+Install-SqlServerExpress2019
+Install-Ssms
+Restore-Database -ServerInstance $ServerInstance -DatabaseName $DatabaseName -BackupUrl $AdventureWorkUrl -UserMssql $UserMssql -MssqlRoot $MssqlRoot
+# Add the user as sysadmin
+Invoke-Sqlcmd -ServerInstance $ServerInstance -Query "EXEC sp_addsrvrolemember '$UserMssql', 'sysadmin'" -TrustServerCertificate
